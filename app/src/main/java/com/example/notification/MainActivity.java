9f04ef72b14ec3d491900136fad22e9b55cdbda8
Alain Wackenheim <alain.wackenheim@protonmail.com>
2020-03-04 17:10:13 +0100

    Initial commit

---
+++ app/src/main/java/com/example/notification/MainActivity.java
@@ -0,0 +1,81 @@
+package com.example.notification;
+
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.Context;
+import android.os.Build;
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.app.NotificationBuilderWithBuilderAccessor;
+
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import java.nio.channels.Channel;
+
+public class MainActivity extends AppCompatActivity {
+
+    NotificationManager notificationManager;
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        NotificationChannel notificationChannel = new NotificationChannel("testchannel","chan", NotificationManager.IMPORTANCE_DEFAULT);
+        notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        notificationManager.createNotificationChannel(notificationChannel);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+                Notification notification = new Notification.Builder(MainActivity.this, "testchannel")
+                        .setContentTitle("TEST")
+                        .setSmallIcon(R.drawable.ic_launcher_background)
+                        .build();
+
+                notificationManager.notify(0, notification);
+
+
+            }
+        });
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
